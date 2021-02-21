490f43966392f3a7f7cf634dbb3cefa7569cdf48
omesser <odedmesser@gmail.com>
2021-02-21 13:27:06 +0200

    M

--- quick-start/background.sh
+++ quick-start/background.sh
@@ -15,10 +15,10 @@
 # limitations under the License.
 
 MLRUN_VERSION=0.6.0
+MLRUN_JUPYTER_VERSION=0.6.1-rc2-jupy
 
 # TODO: remove once 0.6.1 is out
-docker pull quay.io/mlrun/jupyter:0.6.1-rc1
-# docker pull quay.io/mlrun/jupyter:${MLRUN_VERSION}
+docker pull quay.io/mlrun/jupyter:${MLRUN_JUPYTER_VERSION}
 docker pull mlrun/mlrun-api:${MLRUN_VERSION}
 docker pull mlrun/mlrun-ui:${MLRUN_VERSION}
 docker pull mlrun/ml-models:${MLRUN_VERSION}
