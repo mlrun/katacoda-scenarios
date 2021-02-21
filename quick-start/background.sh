cdef22af6eed40efd140cef3a28a0022c132bf5f
omesser <odedmesser@gmail.com>
2021-02-21 15:06:03 +0200

    M2

--- quick-start/background.sh
+++ quick-start/background.sh
@@ -16,7 +16,7 @@
 
 MLRUN_VERSION=0.6.0
 
-# TODO: remove once 0.6.1 is out
+# TODO: change to MLRUN_VERSION once 0.6.1 is out
 docker pull quay.io/mlrun/jupyter:0.6.1-rc2-jupy
 docker pull mlrun/mlrun-api:${MLRUN_VERSION}
 docker pull mlrun/mlrun-ui:${MLRUN_VERSION}
