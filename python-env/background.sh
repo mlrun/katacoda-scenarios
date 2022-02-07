c7a3ddb48dc78dd3b7a291a6cbe605a849872b46
Yaron Haviv <yhaviv@gmail.com>
2022-02-07 02:08:30 +0200

    add scenarios

--- python-env/background.sh
+++ python-env/background.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
 mkdir /root/editor
-echo "Installing MLRun ..."
-pip install git+https://github.com/mlrun/mlrun.git@development
-echo "MLRun Installed"
---
+++ python-env/foreground.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Installing MLRun ..."
+pip install git+https://github.com/mlrun/mlrun.git@development
+echo "MLRun Installed"
