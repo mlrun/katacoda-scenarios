b73f2119a813a7d474300ce5a6fe0d384b161e99
Yaron Haviv <yhaviv@gmail.com>
2022-02-13 14:00:14 +0200

    edit scenarios

--- 00-mlrun-tutorial/foreground.sh
+++ 00-mlrun-tutorial/foreground.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pip install git+https://github.com/mlrun/mlrun.git@development --ignore-installed PyYAML
+pip install mlrun==0.10.0-rc9 --ignore-installed PyYAML
 pip install uvicorn~=0.12.0 apscheduler~=3.6 sqlite3-to-mysql~=1.4
 pip install sklearn plotly~=5.4
 echo "MLRun Installed"
