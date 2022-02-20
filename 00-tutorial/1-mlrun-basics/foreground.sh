e569a23049c14d62b8274d56389e8aaff778cf82
Yaron Haviv <yhaviv@gmail.com>
2022-02-21 01:47:45 +0200

    fix

--- 00-tutorial/1-mlrun-basics/foreground.sh
+++ 00-tutorial/1-mlrun-basics/foreground.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 pip install mlrun[api] --ignore-installed PyYAML
-echo "MLRun Installed"
+pip install sklearn
+echo "MLRun Installed - You can continue now"
 cd editor
--- 00-tutorial/1-mlrun-basics/install_extras.sh
+++ 00-tutorial/1-mlrun-basics/install_extras.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "0" > /root/installed
-pip install sklearn plotly~=5.4
+pip install plotly~=5.4
 echo "1" > /root/installed
 pip install matplotlib seaborn~=0.11.0 scikit-plot~=0.3.7 matplotlib~=3.0
 echo "2" > /root/installed
