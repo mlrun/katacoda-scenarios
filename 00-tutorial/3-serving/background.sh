e153fd89dd302eb0a3234bc31059aa4968f9a432
Yaron Haviv <yhaviv@gmail.com>
2022-02-23 10:51:44 +0200

    fix serving

--- 00-tutorial/3-serving/background.sh
+++ 00-tutorial/3-serving/background.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 echo "start" > /root/state
 docker run -p 8070:8070 -d -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp --name nuclio-dashboard quay.io/nuclio/dashboard:stable-amd64
-MLRUN_NUCLIO_DASHBOARD_URL=http://localhost:8070 mlrun db &
 echo "done" > /root/state
